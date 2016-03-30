208454ff33e58c5cc052e397a441452a4f3b8d8b
ilyakmet <ilyakmet@gmail.com>
2016-03-31 00:47:45 +0300

    v 0.2

--- test.py
+++ test.py
@@ -3,7 +3,6 @@
 import urllib.request
 from socket import timeout
 from bs4 import BeautifulSoup
-import random
 from datetime import datetime
 
 
@@ -95,12 +94,12 @@ def calc_tools(id):
 		calc= []
 		for i in calc_web[0:len(calc_web):2]:
 			row = i.find('tr').text.strip('\n').split('\n') 
-			print(row)
 			if len(row) < 3:
 				if 'не' in row[1]:
 					calc.append((0,0,0))
 				else:
-					calc.append((0,1,0))
+					tcalc = calc.append((0,1,0))
+					print(tcalc)
 			else:
 				calc.append((1,1,row[3]))
 	except(urllib.error.HTTPError, urllib.error.URLError):
@@ -113,66 +112,37 @@ def calc_tools(id):
 		calc = [('nodata', 0, 0), ('nodata', 0, 0), ('nodata', 0, 0), ('nodata', 0, 0), ('nodata', 0, 0), ('nodata', 0, 0), ('nodata', 0, 0)]
 	return calc
 
-'''
 def out(name, total, border):
 	try:
 		f = open(name, 'r')
-		f2 = open(DIR + 'house_percent.csv', 'w')
-		f2.write('id,lat,lon,percent,income,expenditures,external\n')
+		f2 = open(DIR + 'house_parse_data.csv', 'w')
+		f2.write('id,lat,lon,gaz, need, date on,heating, need, date on,power, need, date on,cold, need, date on,water_remove, need, date on,hot, need, date on\n')
 		count = 0
 		for i in f:
 			time = datetime.now()
-			print('operation:', count, '/', total)
-			if count < border:
+			if count <= border:
+				print('operation:', count, '/', border)
 				s = i.split(',')
-				#print(s)
-				percent_row = d_percent(BASE_URL + s[0])
-				#print('percent:', percent_row)
-				if type(percent_row) == type(float()):
-					finance_row = finance(BASE_URL + s[0])
-					f2.write( s[0] + ',' + s[1] + ',' + s[2].strip('\n') + ',' + str(percent_row) + ',' + finance_row['income'] + ',' + finance_row['expenditures'] + ',' + finance_row['external'] + '\n')
-				else:
-					#print('not float')
-					f2.write( s[0] + ',' + s[1] + ',' + s[2].strip('\n') + ',' + str(percent_row) + ',' + 'nodata' + ',' + 'nodata' + ',' + 'nodata' + '\n')
-				count += 1
-				print('speed:', datetime.now() - time)
-	except:
-		f.close()
-		print('ERROR')
-		print('out_total:', count)
-	f.close()
-	print('END')
-	print('out_total:', count)
-'''
-def out(name, total, border):
-	try:
-		f = open(name, 'r')
-		f2 = open(DIR + 'house_percent.csv', 'w')
-		f2.write('id,lat,lon,percent,income,expenditures,external,gaz,on/off, need, date on,heating,on/off, need, date on,power,on/off, need, date on,cold,on/off, need, date on,water_remove,on/off, need, date on,hot,on/off, need, date on\n')
-		count = 0
-		for i in f:
-			time = datetime.now()
-			print('operation:', count, '/', total)
-			if count < border:
-				s = i.split(',')
-				#print(s)
-				calc_tools(s[0])
-				finance_row = finance(BASE_URL + s[0])
-				f2.write( s[0] + ',' + s[1] + ',' + s[2].strip('\n') + ',' + str(d_percent(BASE_URL + s[0])) + ',' + finance_row['income'] + ',' + finance_row['expenditures'] + ',' + finance_row['external'] + '\n')
+				calc = calc_tools(s[0]) 
+				#f2.write( s[0] + ',' + s[1] + ',' + s[2].strip('\n') + ',' + str(d_percent(BASE_URL + s[0])) + '\n')
+				f2.write(s[0] + ',' + s[1] + ',' + s[2].strip('\n') + ',')
+				for i in calc:
+					for j in i:
+						f2.write(str(j) + ',')
+				f2.write('\n')
 				count += 1
 				speed = datetime.now() - time
-				print('speed:', speed, 'wait:', (float(str(speed).split(':')[2])*(total-count))/60/60)
+				print('speed:', speed, 'wait:', (float(str(speed).split(':')[2])*(border-count))/60/60, ' h')			
 	except:
 		f.close()
 		print('ERROR')
-		print('out_total:', count)
+		print('out_total:', count)	
 	f.close()
 	print('END')
-	print('out_total:', count)
+	return count
 
-
-def to_final_csv(name):
-	print('start to_final_csv')
+def housedata_final(name):
+	print('start housedata_final')
 	f = open(name, 'r')
 	f2 = open(DIR + 'housedata_final.csv', 'w')
 	count = 0
@@ -183,52 +153,40 @@ def to_final_csv(name):
 			count += 1
 		except IndexError:
 			print('nodata')
+			count -= 1
 	f2.close()
-	#print('total: ',count)
 	return count
 
-def to_final_csv_errors(name):
-	print('start to_final_csv_errors')
+def to_house_final(name):
+	print('start to_house_final')
 	f = open(name, 'r')
-	f2 = open(DIR + 'housedata_final_errors.csv', 'w')
-	count = 0
+	f2 = open(DIR + 'house_parse_data_errors.csv', 'w')
+	f3 = open(DIR + 'house_final_' + str(datetime.now()) + '_.csv', 'w')
+	errors_count = 0
+	complete_count = 0
 	for i in f:
 		s = i.split(',')
 		print(s)
-		if ('connection error' in s)or('timeout' in s):
-			try:
+		try:
+			if (('timeout' in s)or('connection error' in s)or('timeout\n' in s)or('connection error\n' in s)):
 				f2.write(s[0].strip('\n') + ',' + s[1].strip('\n') + ',' + s[2].strip('\n') + '\n')
-				count += 1
-			except IndexError:
-				print('error')
+				errors_count += 1
+			else:
+				for i in s:
+					f3.write(str(i) + ',')
+				f3.write('\n')
+				complete_count += 1
+		except IndexError:
+			print('error')
 	f2.close()
-	#print('total errors: ',count)
-	return count
-
-def remove_nodata(name):
-	print('start remove_nodata')
-	f = open(name, 'r')
-	f2 = open(DIR + 'house_percent_final_' + str(random.randint(1000,1000000)) + '.csv', 'w')
-	count = 0
-	for i in f:
-		s = i.split(',')
-		print(s)
-		if (('timeout' in s)or('connection error' in s)or('timeout\n' in s)or('connection error\n' in s)) == False:
-			try:
-				f2.write(s[0] + ',' + s[1] + ',' + s[2] + ',' + s[3]  + ',' + s[4] + ',' + s[5] + ',' + s[6])
-				count += 1
-			except IndexError:
-				print('error')
-	f2.close()
-	#print('total nodata: ',count)
-	return count
+	f3.close()
+	return errors_count, complete_count
 
 def main():
-	total = to_final_csv(DIR + 'housedata.csv')
-	out(DIR + MAIN_DATA, total, 10)
-	errors = to_final_csv_errors(DIR + 'house_percent.csv')
-	nodata = remove_nodata(DIR + 'house_percent.csv')
-	print('total:', total, '\n', 'errors:', errors, '\n', 'nodata:', total-nodata, '\n', 'copmlete:', total - errors - nodata, '\n')
+	total = housedata_final(DIR + 'housedata.csv')
+	out_total = out(DIR + MAIN_DATA, total, 100)
+	errors, complete = to_house_final(DIR + 'house_parse_data.csv')
+	print('total:', out_total - 1, '\n', 'errors:', errors - 1, '\n', '\n', 'copmlete:', complete - 1, '\n')
 
 
 if __name__ == '__main__':
